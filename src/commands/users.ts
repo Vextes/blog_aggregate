fc4c92e61474420f4b2e9b010e3df36ddc13a1b7
vextes <stevenfranger@yahoo.com>
2026-03-31 17:17:32 -0500

    add command to register new users

--- src/commands/users.ts
+++ src/commands/users.ts
@@ -1,10 +1,32 @@
 import { setUser } from "src/config"
+import { createUser, getUser } from "src/lib/db/queries/users";
 
-export function handlerLogin(cmdName: string, ...args: string[]) {
+export async function handlerLogin(cmdName: string, ...args: string[]) {
     if (args.length !== 1) {
         throw new Error("please enter a single username")
     }
     const currUser = args[0];
+    const existing = await getUser(currUser);
+    if (!existing) {
+        throw new Error(`User ${currUser} does not exist`);
+    }
+    setUser(existing.name);
+    console.log(`Current user has been set to ${existing.name}`);
+}
+
+export async function handlerRegister(cmdName: string, ...args: string[]) {
+    if (args.length !== 1) {
+        throw new Error("please enter a single username to register")
+    }
+
+    const regUser = args[0];
+    const existing = await getUser(regUser);
+    if (existing) {
+        throw new Error("User has already been registered");
+    }
+    
+    const createdUser = await createUser(regUser);
+    const currUser = createdUser.name;
     setUser(currUser);
     console.log(`Current user has been set to ${currUser}`);
 }
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import { CommandsRegistry, registerCommand, runCommand } from "./commands/commands";
-import { handlerLogin } from "./commands/users";
+import { handlerLogin, handlerRegister } from "./commands/users";
 
-function main() {
+async function main() {
     const cmdReg: CommandsRegistry = {};
     registerCommand(cmdReg, "login", handlerLogin);
+    registerCommand(cmdReg, "register", handlerRegister)
     const args = process.argv.slice(2);
     if (args.length === 0) {
         console.log("No arguments provided")
@@ -11,9 +12,11 @@ function main() {
     }
 
     try {
-        runCommand(cmdReg, args[0], ...args.slice(1));
+        await runCommand(cmdReg, args[0], ...args.slice(1));
+        process.exit(0);
     } catch (error) {
         console.error(`${error}`)
+        process.exit(1);
     }
 }
 
